7c2f8a62aeba2fa88712d051deb450653cbdf002
Steve Bosman <stevebosman@gmail.com>
2023-04-04 22:49:40 +0100

    Fix polar night / midnight sun calculation
    - improve test coverage
    - prep for 0.2.0
    
    Signed-off-by: Steve Bosman <stevebosman@gmail.com>

--- src/main/kotlin/SunriseDetails.kt
+++ src/main/kotlin/SunriseDetails.kt
@@ -3,9 +3,14 @@ package uk.co.stevebosman.sunrise
 import java.time.ZonedDateTime
 
 class SunriseDetails(
-    val daylightType: DaylightType,
+    val sunriseType: DaylightType,
+    val sunsetType: DaylightType,
     val solarNoonTime: ZonedDateTime,
     val sunriseTime: ZonedDateTime,
     val sunsetTime: ZonedDateTime,
     val moonPhase: Double
-)
+) {
+    override fun toString(): String {
+        return "SunriseDetails(sunriseType=$sunriseType, sunsetType=$sunsetType, solarNoonTime=$solarNoonTime, sunriseTime=$sunriseTime, sunsetTime=$sunsetTime, moonPhase=$moonPhase)"
+    }
+}
